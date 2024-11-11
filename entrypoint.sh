35401b96403466976d4d8011c70dff3a3e4f2bb4
tonypowa <tonypowa@gmail.com>
2024-11-11 19:39:06 +0100

    pythona app issue fix3

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Run the test script
 echo "Starting test script..."
-python3 test.py &
+sudo python3 test.py &
 
 # Run the main application
 echo "Starting main application..."
-python3 app.py
+sudo python3 app.py
